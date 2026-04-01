5aa77c591a0cebc560bb5fb488ccf766952e6b19
krinaldholakiya <krinaldholakiya@gmail.com>
2026-04-01 11:38:03 +0530

    Add files via upload

---
+++ practice 18.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	printf("enter number");
+	scanf("%d",&x);
+	
+	if(x>99 && x<1000)
+	{
+		printf("x is three digit number");
+	}
+	else
+	{
+		printf("x is not three digit number");
+	}
+}
+
+
+
+
+
+
+
+
